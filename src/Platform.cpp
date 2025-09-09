934e3507ba5b541b607ec90f7775de84f84fae9f
unknown <vuco80@gmail.com>
2025-09-09 18:19:01 +0200

    platform

---
+++ src/Platform.cpp
@@ -0,0 +1,136 @@
+#include "Platform.h"
+
+namespace vuk {
+
+IPlatformEventListener::IPlatformEventListener(){}
+IPlatformEventListener::~IPlatformEventListener(){}
+
+IPlatformFrameListener::IPlatformFrameListener(){}
+IPlatformFrameListener::~IPlatformFrameListener(){}
+
+Platform* Platform::m_instance = nullptr;
+
+Platform::Platform() : m_window(nullptr), m_renderer(nullptr), m_updateStepMs(16) {}
+Platform::~Platform(){}
+
+Platform* Platform::instance() {
+	if(m_instance == nullptr) m_instance = new Platform();
+	return m_instance;
+}
+	
+int Platform::createAndShowWindow(const std::string& caption) {
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init();
+	TTF_Init();
+	
+	m_window = SDL_CreateWindow(caption.c_str(), 800, 600, 0);
+	if(m_window == nullptr) return 1;
+	
+	m_renderer = SDL_CreateRenderer(m_window, 0);
+	if(m_renderer == nullptr) return 2;
+	
+	return 0;
+}
+
+void Platform::quit() {
+	if(m_renderer != nullptr) SDL_DestroyRenderer(m_renderer);
+	if(m_window != nullptr) SDL_DestroyWindow(m_window);
+}
+
+int Platform::pollEvents(IPlatformEventListener* listener) {
+	SDL_Event evt;
+	while(SDL_PollEvent(&evt)) {
+		listener->OnPlatformEvent(&evt);
+	}
+}
+
+int Platform::loadTexture(const std::string& filename, SDL_Texture** dst, int* width, int* height) {
+	SDL_Surface* s = IMG_Load(filename.c_str());
+	if(s == 0) return 1;
+	
+	*dst = SDL_CreateTextureFromSurface(m_renderer, s);
+	if(*dst == 0) {
+		SDL_DestroySurface(s);
+		return 2;
+	}
+	*width = s->w;
+	*height = s->h;
+	SDL_DestroySurface(s);
+	
+	return 0;
+}
+
+int Platform::createTexture(SDL_Texture** dst, int width, int height) {
+	*dst = SDL_CreateTexture(m_renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 
+		width, height);
+	if(*dst == nullptr) return 1;
+	return 0;
+}
+
+void Platform::destroyTexture(SDL_Texture** texture) {
+	SDL_DestroyTexture(*texture);
+	*texture = nullptr;
+}
+
+int Platform::setRenderTarget(SDL_Texture* target) {
+	if(SDL_SetRenderTarget(m_renderer, target)) 
+		return 0;
+	return 1;
+}
+
+void Platform::renderTexture(SDL_Texture* texture, SDL_FRect* src, SDL_FRect* dst) {
+	SDL_RenderTexture(m_renderer, texture, src, dst);
+}
+
+int Platform::loadFont(const std::string& filename, int dimension, int fontId) {
+	if(m_fonts.find(fontId) != m_fonts.end()) return 1;
+	
+	TTF_Font* font = TTF_OpenFont(filename.c_str(), dimension);
+	
+	if(font == nullptr) return 2;
+	
+	m_fonts[fontId] = font;
+	
+	return 0;
+}
+
+int Platform::createTextTexture(int fontId, const std::string& text, const SDL_Color& color, int maxWidth, SDL_Texture** dst, int* width, int* height) {
+	if(m_fonts.find(fontId) == m_fonts.end()) return 1;
+	
+	TTF_Font* font = m_fonts[fontId];
+	SDL_Surface* s = TTF_RenderText_Blended_Wrapped(font, text.c_str(), text.length(), color, maxWidth);
+	
+	if(s == nullptr) return 2;
+	
+	*dst = SDL_CreateTextureFromSurface(m_renderer, s);
+	if(*dst == nullptr) return 3;
+			
+	*width = s->w;
+	*height = s->h;
+	
+	SDL_DestroySurface(s);
+	
+	return 0;
+}
+
+void Platform::initFrameTimer(int updateStepMs) {
+	m_frameTime = SDL_GetTicks();
+	m_frameTimeAccumulator = 0;
+	m_updateStepMs = updateStepMs;
+}
+
+void executeFrame(IPlatformFrameListener* listener);
+	Uint32 now = SDL_GetTicks();
+	Uint32 elapsed = now - m_frameTime;
+	m_frameTime = now;
+	m_frameTimeAccumulator += elapsed;
+	
+	while(m_frameTimeAccumulator >= m_updatestepMs) {
+		m_frameTimeAccumulator -= m_updatestepMs;
+		listener->OnFrameUpdateStep();
+	}
+	
+	SDL_RenderClear(m_renderer);
+	listener->OnFrameRender(elapsed);
+	SDL_RenderPresent(m_renderer);
+};
---
+++ src/Platform.h
@@ -0,0 +1,67 @@
+#ifndef VUK_PLATFORM_H
+#include UK_PLATFORM_H
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_image.h>
+#include <SDL3/SDL_ttf.h>
+#include <map>
+#include <string>
+
+namespace vuk {
+	
+class IPlatformEventListener {
+public:
+	IPlatformEventListener();
+	virtual ~IPlatformEventListener();
+	
+	virtual void OnEvent(const SDL_Event* evt) = 0;
+};
+
+class IPlatformFrameListener {
+public:
+	IPlatformFrameListener();
+	virtual ~IPlatformFrameListener();
+	
+	virtual void OnFrameUpdateStep() = 0;
+	virtual void OnFrameRender(unsigned int elapsedMs) = 0;
+};
+
+class Platform {
+public:
+	~Platform();
+	
+	static Platform* instance();
+	
+	int createAndShowWindow(const std::string& caption);
+	void quit();
+	
+	int pollEvents(IPlatformEventListener* listener);
+	
+	int loadTexture(const std::string& filename, SDL_Texture** dst, int* width, int* height);
+	int createTexture(SDL_Texture** dst, int width, int height);
+	void destroyTexture(SDL_Texture** texture);
+	
+	int setRenderTarget(SDL_Texture* target);
+	void renderTexture(SDL_Texture* texture, SDL_FRect* src, SDL_FRect* dst);
+	
+	int loadFont(const std::string& filename, int dimension, int fontId);
+	int createTextTexture(int fontId, const std::string& text, const SDL_Color& color, int maxWidth, SDL_Texture** dst, int* width, int* height);
+	
+	void initFrameTimer(int updateStepMs);
+	void executeFrame(IPlatformFrameListener* listener);
+	
+private:
+	Platform();
+	
+	static Platform* m_instance;
+	SDL_Window* m_window;
+	SDL_Renderer* m_renderer;
+	std::map<int, TTF_Font*> m_fonts;
+	int m_updateStepMs;
+	Uint32 m_frameTime;
+	Uint32 m_frameTimeAccumulator;
+};
+
+};
+
+#endif
